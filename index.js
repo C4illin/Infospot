45cdb8bfb8c5758ffd9c0e1b4a5444d6e5167144
C4illin <emrik.ostling@gmail.com>
2023-06-02 03:49:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const cors = require("cors");
+const SpotifyWebApi = require("spotify-web-api-node");
+const livereload = require("livereload");
+const connectLiveReload = require("connect-livereload");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const url = process.env.URL || "http://localhost:3000";
+
+//Livereload code
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "views"));
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+app.use(connectLiveReload());
+//End of livereload code
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors())
+app.set("view engine", "ejs")
+
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret,
+  redirectUri: url+"/callback",
+});
+
+app.get("/", (req, res) => {
+  if (!spotifyApi.getAccessToken()) {
+    res.redirect("/login");
+    return;
+  }
+
+  let data = {};
+  spotifyApi.getMyCurrentPlayingTrack().then((track) => {
+    data.track = track.body;
+    let songId = track.body.item.id;
+    spotifyApi.getAudioFeaturesForTrack(songId).then((features) => {
+      data.features = features.body;
+    })
+      .catch((err) => {
+        console.log(err);
+        res.send("Error, try again.");
+      });
+
+    spotifyApi.getAudioAnalysisForTrack(songId).then((analysis) => {
+      data.analysis = analysis.body;
+      res.render("index", { "data": data })
+    })
+      .catch((err) => {
+        console.log(err);
+        res.send("Error, try again.");
+      })
+  })
+    .catch((err) => {
+      console.log(err);
+      res.send("Play something on Spotify! And reload the page.");
+    });
+});
+
+app.get("/color", (req, res) => {
+
+})
+
+app.get("/login", (req, res) => {
+  const scopes = [
+    "user-read-private",
+    "user-read-email",
+    "user-top-read",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-modify-playback-state",
+    "user-read-currently-playing",
+    "streaming",
+  ];
+  const authorizeURL = spotifyApi.createAuthorizeURL(scopes);
+  res.redirect(authorizeURL);
+});
+
+app.get("/callback", (req, res) => {
+  const { code } = req.query;
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      const { access_token, refresh_token } = data.body;
+      spotifyApi.setAccessToken(access_token);
+      spotifyApi.setRefreshToken(refresh_token);
+      // Redirect to new route
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/");
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port} and ${url}`);
+});
---
+++ public/script.js
@@ -0,0 +1,194 @@
+const analysis = (data, progress_ms) => {
+  const barsElem = document.getElementById("bars");
+  const beatsElem = document.getElementById("beats");
+  const tatumsElem = document.getElementById("tatums");
+  const bars = data.bars;
+  const beats = data.beats;
+  const tatums = data.tatums;
+  const segments = data.segments;
+
+  tatums.forEach((beat, index) => {
+    const start = beat.start - progress / 1000;
+    const end = start + beat.duration;
+
+    if (start < 0) {
+      return;
+    }
+
+    setTimeout(() => {
+      tatumsElem.style.backgroundColor = "";
+    }, start * 1000 + 50);
+
+    setTimeout(() => {
+      tatumsElem.style.backgroundColor = "white";
+    }, end * 1000);
+  });
+
+  beats.forEach((beat, index) => {
+    const start = beat.start - progress / 1000;
+    const end = start + beat.duration;
+
+    if (start < 0) {
+      return;
+    }
+
+    setTimeout(() => {
+      beatsElem.style.backgroundColor = "";
+    }, start * 1000 + 50);
+
+    setTimeout(() => {
+      beatsElem.style.backgroundColor = "white";
+    }, end * 1000);
+  });
+
+  bars.forEach((bar, index) => {
+    const start = bar.start - progress / 1000;
+    const end = start + bar.duration;
+
+    if (start < 0) {
+      return;
+    }
+
+    setTimeout(() => {
+      barsElem.style.backgroundColor = "";
+    }, start * 1000 + 50);
+
+    setTimeout(() => {
+      barsElem.style.backgroundColor = "white";
+    }, end * 1000);
+  });
+
+  const pitchesData = {
+    labels: ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"],
+    datasets: [{
+      label: "Pitches",
+      data: segments[0].pitches,
+      backgroundColor: "rgba(255, 99, 132, 0.2)",
+      borderColor: "rgba(255, 99, 132, 1)",
+      borderWidth: 1
+    }]
+  };
+
+  const timbreData = {
+    labels: ["dB", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+    datasets: [{
+      label: "Timbre",
+      data: segments[0].timbre,
+      backgroundColor: "rgba(99, 132, 255, 0.2)",
+      borderColor: "rgba(99, 132, 255, 1)",
+      borderWidth: 1
+    }]
+  }
+
+  const pitchesOptions = {
+    animation: {
+      duration: 500
+    },
+    color: txtColor,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+
+  const timbreOptions = {
+    animation: {
+      duration: 500
+    },
+    color: txtColor,
+    scales: {
+      y: {
+        min: -200,
+        max: 200,
+      }
+    }
+  }
+
+  const pitchesChart = new Chart(document.getElementById("chart1"), {
+    type: "bar",
+    data: pitchesData,
+    options: pitchesOptions
+  });
+
+  const timbreChart = new Chart(document.getElementById("chart2"), {
+    type: "bar",
+    data: timbreData,
+    options: timbreOptions
+  });
+
+  const updateChart = (pitches, timbre) => {
+    pitchesChart.data.datasets[0].data = pitches
+    timbreChart.data.datasets[0].data = timbre
+    pitchesChart.update();
+    timbreChart.update();
+  };
+
+  segments.forEach((segment, index) => {
+    const start = segment.start - progress / 1000;
+
+    if (start < 0) {
+      return;
+    }
+
+    setTimeout(() => {
+      updateChart(segment.pitches, segment.timbre)
+    }, start * 1000 + 50);
+  });
+}
+
+let progress = 0;
+const updateProgress = (timestamp, duration, element) => {
+  progress = (Date.now() - timestamp);
+  element.textContent = `${formatMs(progress)} / ${formatMs(duration)}`;
+
+  if (progress >= duration) {
+    location.reload();
+    return;
+  }
+
+  setTimeout(() => {
+    updateProgress(timestamp, duration, element);
+  }, 10);
+}
+
+const formatMs = (ms) => {
+  let minutes = Math.floor(ms / 60000);
+  let seconds = ((ms % 60000) / 1000).toFixed(0);
+  let milliseconds = ms % 1000;
+  return `${minutes}:${(seconds < 10 ? "0" : "")}${seconds}:${(milliseconds < 100 ? "0" : "")}${(milliseconds < 10 ? "0" : "")}${milliseconds}`;
+}
+
+const colorThief = new ColorThief();
+const img = document.querySelector('img');
+
+let txtColor = "white";
+
+const colorHandler = (color) => {
+
+  if (color[0] > 200 && color[1] > 200 && color[2] > 200) {
+    document.documentElement.style.setProperty('--text', 'black');
+    txtColor = "black";
+  } else {
+    document.documentElement.style.setProperty('--text', 'white');
+  }
+
+  document.documentElement.style.setProperty('--bkg', 'rgb(' + color[0] + ',' + color[1] + ',' + color[2] + ')');
+}
+
+if (img.complete) {
+  colorHandler(colorThief.getColor(img));
+} else {
+  img.addEventListener('load', function () {
+    colorHandler(colorThief.getColor(img));
+  });
+}
+
+const getData = (data) => {
+  const progresstext = document.getElementById("progresstext");
+
+  updateProgress(data.track.timestamp, data.track.item.duration_ms, progresstext);
+  analysis(data.analysis, data.track.progress_ms);
+}
